790b9598023927a70945dfe826c36a2521576cf5
Marc Brouwers <mgbrouwers@gmail.com>
2018-02-02 16:03:54 +0100

    yeye

--- V2.1.py
+++ V2.1.py
@@ -44,12 +44,15 @@ def main():
         xjup = np.array([R_jup,0,0])
         vjup = np.array([0,V_jup,0])
         
-        theta = 2*m.pi*np.random.rand(1,nbodies)[0]      #Phase of the orbits
-        r = np.random.uniform(R_earth, high=R_jup, size=nbodies)
-        v = np.sqrt(G*M_sun/r) # this is pure keplerian, no random velocities
+        #theta = 2*m.pi*np.random.rand(1,nbodies)[0]      #Phase of the orbits
+        e = np.random.uniform(0, .05, size=nbodies)
+        r = np.random.uniform(R_earth, R_jup, size=nbodies)
+        x = r * (1 + e)
+        y = np.zeros(nbodies)
         z = np.zeros(nbodies) # for now no z_values, otherwise make phi variable
-        xast = np.stack((r*np.cos(theta),r*np.sin(theta), z), axis=-1)
-        vast = np.stack((v*np.cos(theta+m.pi/2),v*np.sin(theta+m.pi/2), z), axis=-1)
+        v = np.sqrt(G*M_sun/x * (1 + e) / (1 - e)) # this is pure keplerian, no random velocities
+        xast = np.stack((x, y, z), axis=-1)
+        vast = np.stack((0 * v, v, 0 * v), axis=-1)
         return xast, vast, xsun, vsun, xjup, vjup
 
     
@@ -71,43 +74,47 @@ def main():
         time = timer()
         for i in range(t_intervals):
             xast, vast = EulerCromer_gravitational(xast,vast,xsun, xjup, dt)
-            vast = vast[np.amax(np.absolute(xast[:]),axis=1)<10*R_jup]
-            xast = xast[np.amax(np.absolute(xast[:]),axis=1)<10*R_jup]
+            vast = vast[np.amax(np.absolute(xast[:]),axis=1)<2*R_jup]
+            xast = xast[np.amax(np.absolute(xast[:]),axis=1)<2*R_jup]
             xsun, vsun = EulerCromer_gravitational(xsun,vsun,xsun, xjup, dt)
             xjup, vjup = EulerCromer_gravitational(xjup,vjup,xsun, xjup, dt)
             if (i%(t_intervals/100)==0) and i !=0:
                 print str(i*100./t_intervals)+"%" + '  dt:  ' + str((timer() - time) /  i * (t_intervals - i)), xast.shape
+        return xast, xsun, xjup
 
     M_jup_scaling = 1.898e+27 # normalization factor for mass
-    R_jup_scaling = 7.785e+11 # normalization factor for length
+#   R_jup_scaling = 7.785e+11 # normalization factor for length
     year_scaling = 365 * 24 * 3600 # normalization factor for time
-    G = 6.67408e-11 * R_jup_scaling**(-3) * M_jup_scaling * year_scaling**2
+    AU_scaling = 1.496e+11
+    G = 6.67408e-11 * AU_scaling**(-3) * M_jup_scaling * year_scaling**2
     M_sun = 1.989e+30 / M_jup_scaling
-    R_jup = 1.
+    R_jup = 5.2
     M_jup = 1.
-    V_jup = G*M_sun/ R_jup
-    R_earth = 1.496e+11 / R_jup_scaling
+    V_jup = m.sqrt(G*M_sun/ R_jup)
+    R_earth = 1.
     
-    nbodies = 2500
+    nbodies = 25000
     dt = 1./365 # in years
     t_end = 100 # in years
     t_intervals = int(t_end / dt)
-
     
     
     start = timer()
     xast, vast, xsun, vsun, xjup, vjup = init()
-    integrator(xast, vast, xsun, vsun, xjup, vjup)       
+    xast, xsun, xjup = integrator(xast, vast, xsun, vsun, xjup, vjup)
     duration = timer() - start   
     print 'actual simulation time: ' + str(duration)
     
-    print 'plotting...'
     plt.figure(figsize=(8,6))
     plt.plot(xsun[0], xsun[1], 'o', c = 'y') 
     plt.plot(xjup[0], xjup[1], 'o', c = 'b') 
     plt.plot(xast[:,0], xast[:,1], 'o', c = 'g', markersize = 1)  
+
+    plt.figure(figsize=(8,6))
+    r_asteroids = np.sqrt(xast[:,0]**2 + xast[:,1]**2)
+    n, bins, patches = plt.hist(r_asteroids, 1000, facecolor='green', alpha=0.75)
+    plt.xlim(2,3.5)
+    
     plt.show()
-
-
 if __name__ == '__main__':
     main()
